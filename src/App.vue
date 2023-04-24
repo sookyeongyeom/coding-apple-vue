66259484b9a9dd6d1fd0a41296e9381f417b0114
sookyeongyeom <sookyeong.dev@gmail.com>
2023-04-24 20:00:20 +0900

    :sparkles: event handler

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="menu">
+    <a v-for="menu in menus" :key="menu">{{ menu }}</a>
+  </div>
+  <div v-for="(product, i) in products" :key="i">
+    <h4>{{ product.name }}</h4>
+    <p>{{ product.price }}만원</p>
+    <button @click="() => increaseReportCnt(i)">허위매물신고</button> <span>신고수 {{ product.reportCnt }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      price1: 60,
+      price2: 70,
+      price3: 80,
+      products: [
+        { name: '역삼동원룸', price: 60, reportCnt: 0 },
+        { name: '천호동원룸', price: 70, reportCnt: 0 },
+        { name: '마포구원룸', price: 80, reportCnt: 0 },
+      ],
+      menus: ['Home', 'Products', 'About'],
+    };
+  },
+  methods: {
+    increaseReportCnt(idx) {
+      this.products[idx].reportCnt++;
+    },
+  },
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+</style>
