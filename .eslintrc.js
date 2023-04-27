ce937b6dc1158f92dd52a6184c898a19bded2381
sookyeongyeom <sookyeong.dev@gmail.com>
2023-04-27 21:02:13 +0900

    :wrench: eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+  },
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/html-self-closing': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/v-on-event-hyphenation': 'off',
+  },
+};
